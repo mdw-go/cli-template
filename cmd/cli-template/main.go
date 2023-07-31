9e35588cf0f1647283f078d6a33367f119deef23
Michael Whatcott <mike@smarty.com>
2023-07-31 16:29:36 -0600

    Better resolution of command name for 'go run ...'.

--- cmd/cli-template/main.go
+++ cmd/cli-template/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var Version = "dev"
@@ -12,6 +13,8 @@ var Version = "dev"
 func main() {
 	log.SetFlags(log.Ltime | log.Lshortfile)
 
-	flags := flag.NewFlagSet(fmt.Sprintf("%s @ %s", os.Args[0], Version), flag.ExitOnError)
+	flags := flag.NewFlagSet(fmt.Sprintf("%s @ %s", filepath.Base(os.Args[0]), Version), flag.ExitOnError)
 	_ = flags.Parse(os.Args[1:])
+
+	fmt.Println(flags.Name())
 }
