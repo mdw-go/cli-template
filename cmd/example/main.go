db4b75799c3fccf4e35b29e5e59531c98fa02125
Michael Whatcott <mike@smarty.com>
2023-08-01 15:10:25 -0600

    Installed additional libraries.

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/mdwhatcott/exec"
+	"github.com/mdwhatcott/must/must"
+	"github.com/mdwhatcott/slogging"
 )
 
 var Version = "dev"
 
 func main() {
-	log.SetFlags(log.Ltime | log.Lshortfile)
+	slogging.SetScriptingLogger()
 
 	flags := flag.NewFlagSet(fmt.Sprintf("%s @ %s", filepath.Base(os.Args[0]), Version), flag.ExitOnError)
 	_ = flags.Parse(os.Args[1:])
 
-	fmt.Println(flags.Name())
+	fmt.Println(must.Value(exec.Run(fmt.Sprintf("echo 'Hello, %s'", flags.Name()))))
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module github.com/mdwhatcott/cli-template
 
-go 1.20
+go 1.21
 
 require (
+	github.com/mdwhatcott/exec v1.0.0
 	github.com/mdwhatcott/funcy v1.7.0
 	github.com/mdwhatcott/go-set/v2 v2.0.1
 	github.com/mdwhatcott/must v1.1.0
+	github.com/mdwhatcott/slogging v1.0.0
 	github.com/mdwhatcott/testing v1.2.0
 )
